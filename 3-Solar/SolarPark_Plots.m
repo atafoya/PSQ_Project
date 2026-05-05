598ff9e97c41bc5cfb4831d184a5afa2692ffa1c
atafoya <abetafoya567@gmail.com>
2026-05-05 01:22:48 -0600

    Solar BESS works!!!

--- 3-Solar/SolarPark_Plots.m
+++ 3-Solar/SolarPark_Plots.m
@@ -1,17 +1,18 @@
 %plotting the v vs i for the solar park
 vbase=4160/sqrt(3);
-ibase=(400*10^3)/(sqrt(3)*4160);
-va_solar=V_solar.Data(:,1)/(sqrt(2)*vbase);
-vb_solar=V_solar.Data(:,2)/(sqrt(2)*vbase);
-vc_solar=V_solar.Data(:,3)/(sqrt(2)*vbase);
+ibase=(1*10^6)/(sqrt(3)*4160);
+va_solar=V_RMS.Data(:,1)/vbase;
+vb_solar=V_RMS.Data(:,2)/vbase;
+vc_solar=V_RMS.Data(:,3)/vbase;
 
-ia_solar=I_solar.Data(:,1)/(sqrt(2)*ibase);
-ib_solar=I_solar.Data(:,2)/(sqrt(2)*ibase);
-ic_solar=I_solar.Data(:,3)/(sqrt(2)*ibase);
+%ia_solar=I_RMS.Data(:,1)/ibase;
+ia_solar=I_RMS.Data(:,1)/ibase;
+ib_solar=I_RMS.Data(:,2)/ibase;
+ic_solar=I_RMS.Data(:,3)/ibase;
 
 figure;
 hold on;
-plot(va_solar, ia_solar, 'r', 'DisplayName', 'Phase A');
+plot(va_solar(335:end), ia_solar(335:end), 'r', 'DisplayName', 'Phase A');
 %plot(vb_solar, ib_solar, 'g', 'DisplayName', 'Phase B');
 %plot(vc_solar, ic_solar, 'b', 'DisplayName', 'Phase C');
 xlabel('Voltage (pu)');
